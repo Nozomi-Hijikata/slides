76117bbac4d9711452deacb9401b291bb42235ee
Nozomi-Hijikata <nozomi.hijikata@techouse.jp>
2026-03-24 18:05:40 +0900

    add slide number

---
+++ shared/components/SlideNumber.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="slide-number">
+    <SlideCurrentNo />
+  </div>
+</template>
+
+<style>
+.slide-number {
+  position: absolute;
+  bottom: 1rem;
+  right: 2rem;
+  font-size: 0.75rem;
+  color: #999;
+  opacity: 0.7;
+}
+</style>
--- shared/components/index.ts
+++ shared/components/index.ts
@@ -1,1 +1,2 @@
 export { default as Footnotes } from './Footnotes.vue'
+export { default as SlideNumber } from './SlideNumber.vue'
